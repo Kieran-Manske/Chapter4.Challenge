0a3dfb2cb7530b87352f9022fe36bd744dd8ba63
Kieran Mansk <kmanske343@lsr7.net>
2023-09-11 09:29:26 -0500

    Update TypingGame.py
    Got window up

--- TypingGame.py
+++ TypingGame.py
@@ -1,1 +1,33 @@
 import pygame
+import random  
+import time
+import sys
+# Define the background colour
+# using RGB color coding.
+background_colour = (234, 212, 252)
+  
+# Define the dimensions of
+# screen object(width,height)
+screen = pygame.display.set_mode((800, 800))
+  
+# Set the caption of the screen
+pygame.display.set_caption('Typing Game')
+  
+# Fill the background colour to the screen
+screen.fill(background_colour)
+  
+# Update the display using flip
+pygame.display.flip()
+  
+# Variable to keep our game loop running
+running = True
+  
+# game loop
+while running:
+    
+# for loop through the event queue  
+    for event in pygame.event.get():
+      
+        # Check for QUIT event      
+        if event.type == pygame.QUIT:
+            running = False
